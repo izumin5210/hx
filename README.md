6dc57fa8b4036272c9bcdf36d0b5d8fb891e7ee3
izumin5210 <m@izum.in>
2019-11-06 23:40:01 +0900

    Add `CloneTransport`

--- README.md
+++ README.md
@@ -36,20 +36,7 @@ err := hx.Get(ctx, "https://api.example.com/contents/1",
 
 ```go
 func init() {
-	// https://github.com/golang/go/blob/go1.13.4/src/net/http/transport.go#L42-L54
-	defaultTransport := &http.Transport{
-		Proxy: ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 30 * time.Second,
-			DualStack: true,
-		}).DialContext,
-		ForceAttemptHTTP2:     true,
-		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-	}
+	defaultTransport := hx.CloneTransport(http.DefaultTransport)
 
 	// Tweak keep-alive configuration
 	defaultTransport.MaxIdleConns = 500
--- helper.go
+++ helper.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/url"
 	"path"
+	"reflect"
 	"strings"
 )
 
@@ -22,6 +23,25 @@ func DrainResponseBody(r *http.Response) error {
 	}
 	r.Body = ioutil.NopCloser(&buf)
 	return nil
+}
+
+// CloneTransport creates a new *http.Transport object that has copied attributes from a given one.
+func CloneTransport(in *http.Transport) *http.Transport {
+	out := new(http.Transport)
+	outRv := reflect.ValueOf(out).Elem()
+
+	rv := reflect.ValueOf(in).Elem()
+	rt := rv.Type()
+
+	n := rt.NumField()
+	for i := 0; i < n; i++ {
+		src, dst := rv.Field(i), outRv.Field(i)
+		if src.Type().AssignableTo(dst.Type()) && dst.CanSet() {
+			dst.Set(src)
+		}
+	}
+
+	return out
 }
 
 func Path(elem ...interface{}) string {
