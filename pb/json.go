92d3b9cf8d8dc6f9aa716b77de25937777bf33b3
izumin5210 <m@izum.in>
2019-11-08 09:49:52 +0900

    Make encode and decoder configurable

--- pb/json.go
+++ pb/json.go
@@ -12,18 +12,36 @@ import (
 )
 
 func JSON(pb proto.Message) hx.Option {
-	return hx.OptionFunc(func(c *hx.Config) {
-		c.BodyOption = jsonEncoder(pb)
-	})
+	return (&JSONConfig{}).JSON(pb)
 }
 
 func AsJSON(pb proto.Message) hx.ResponseHandler {
+	return (&JSONConfig{}).AsJSON(pb)
+}
+
+type JSONConfig struct {
+	jsonpb.Marshaler
+	jsonpb.Unmarshaler
+	EncodeFunc func(proto.Message) (io.Reader, error)
+	DecodeFunc func(io.Reader, proto.Message) error
+}
+
+func (c *JSONConfig) JSON(pb proto.Message) hx.Option {
+	return hx.OptionFunc(func(hc *hx.Config) {
+		hc.BodyOption = func(context.Context) (io.Reader, error) {
+			return c.encode(pb)
+		}
+	})
+}
+
+func (c *JSONConfig) AsJSON(pb proto.Message) hx.ResponseHandler {
 	return func(r *http.Response, err error) (*http.Response, error) {
 		if r == nil || err != nil {
 			return r, err
 		}
+
 		defer r.Body.Close()
-		err = (&jsonpb.Unmarshaler{}).Unmarshal(r.Body, pb)
+		err = c.decode(r.Body, pb)
 		if err != nil {
 			return nil, err
 		}
@@ -31,13 +49,23 @@ func AsJSON(pb proto.Message) hx.ResponseHandler {
 	}
 }
 
-func jsonEncoder(pb proto.Message) func(context.Context) (io.Reader, error) {
-	return func(ctx context.Context) (io.Reader, error) {
-		var buf bytes.Buffer
-		err := (&jsonpb.Marshaler{}).Marshal(&buf, pb)
-		if err != nil {
-			return nil, err
-		}
-		return &buf, nil
+func (c *JSONConfig) encode(pb proto.Message) (io.Reader, error) {
+	if f := c.EncodeFunc; f != nil {
+		return f(pb)
 	}
+
+	var buf bytes.Buffer
+	err := c.Marshal(&buf, pb)
+	if err != nil {
+		return nil, err
+	}
+	return &buf, nil
+}
+
+func (c *JSONConfig) decode(r io.Reader, pb proto.Message) error {
+	if f := c.DecodeFunc; f != nil {
+		return f(r, pb)
+	}
+
+	return c.Unmarshal(r, pb)
 }
--- pb/proto.go
+++ pb/proto.go
@@ -11,23 +11,34 @@ import (
 )
 
 func Proto(pb proto.Message) hx.Option {
-	return hx.OptionFunc(func(c *hx.Config) {
-		c.BodyOption = protoEncoder(pb)
-	})
+	return (&ProtoConfig{}).Proto(pb)
 }
 
 func AsProto(pb proto.Message) hx.ResponseHandler {
+	return (&ProtoConfig{}).AsProto(pb)
+}
+
+type ProtoConfig struct {
+	EncodeFunc func(proto.Message) (io.Reader, error)
+	DecodeFunc func(io.Reader, proto.Message) error
+}
+
+func (c *ProtoConfig) Proto(pb proto.Message) hx.Option {
+	return hx.OptionFunc(func(hc *hx.Config) {
+		hc.BodyOption = func(context.Context) (io.Reader, error) {
+			return c.encode(pb)
+		}
+	})
+}
+
+func (c *ProtoConfig) AsProto(pb proto.Message) hx.ResponseHandler {
 	return func(r *http.Response, err error) (*http.Response, error) {
 		if r == nil || err != nil {
 			return r, err
 		}
+
 		defer r.Body.Close()
-		var buf bytes.Buffer
-		_, err = io.Copy(&buf, r.Body)
-		if err != nil {
-			return nil, err
-		}
-		err = proto.Unmarshal(buf.Bytes(), pb)
+		err = c.decode(r.Body, pb)
 		if err != nil {
 			return nil, err
 		}
@@ -35,12 +46,27 @@ func AsProto(pb proto.Message) hx.ResponseHandler {
 	}
 }
 
-func protoEncoder(pb proto.Message) func(context.Context) (io.Reader, error) {
-	return func(ctx context.Context) (io.Reader, error) {
-		data, err := proto.Marshal(pb)
-		if err != nil {
-			return nil, err
-		}
-		return bytes.NewReader(data), nil
+func (c *ProtoConfig) encode(pb proto.Message) (io.Reader, error) {
+	if f := c.EncodeFunc; f != nil {
+		return f(pb)
 	}
+
+	data, err := proto.Marshal(pb)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewReader(data), nil
+}
+
+func (c *ProtoConfig) decode(r io.Reader, pb proto.Message) error {
+	if f := c.DecodeFunc; f != nil {
+		return f(r, pb)
+	}
+
+	var buf bytes.Buffer
+	_, err := io.Copy(&buf, r)
+	if err != nil {
+		return err
+	}
+	return proto.Unmarshal(buf.Bytes(), pb)
 }
