ce4e12d92a1b58e5cefb3eb3fb38ffe608a5f155
izumin5210 <m@izum.in>
2019-11-12 09:08:51 +0900

    [retry] Add doc

--- retry/options.go
+++ retry/options.go
@@ -1,3 +1,4 @@
+// A plugin for retry HTTP requests.
 package retry
 
 import (
@@ -7,6 +8,17 @@ import (
 	"github.com/izumin5210/hx"
 )
 
+// When creates an option that provides retry mechanism for your http client.
+//  bo := backoff.NewExponentialBackOff()
+//  bo.InitialInterval = 50 * time.Millisecond
+//  bo.MaxInterval = 500 * time.Millisecond
+//
+//  err := hx.Post(ctx, "https://example.com/api/messages",
+//  	retry.When(hx.Any(hx.IsServerError(), hx.IsTemporaryError()), bo),
+//  	hx.JSON(&in),
+//  	hx.WhenSuccess(hx.AsJSON(&out)),
+//  	hx.WhenFailure(hx.AsError()),
+//  )
 func When(cond hx.ResponseHandlerCond, bo backoff.BackOff) hx.Option {
 	return hx.TransportFrom(func(t http.RoundTripper) http.RoundTripper {
 		return NewTransport(t, cond, bo)
