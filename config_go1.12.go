919be6406e16dc50d413a42a65de2ee7b445ac99
izumin5210 <m@izum.in>
2019-11-13 13:12:29 +0900

    Fix error for go1.12

--- config_go1.12.go
+++ config_go1.12.go
@@ -10,7 +10,7 @@ import (
 
 func init() {
 	newRequest = func(ctx context.Context, meth, url string, body io.Reader) (*http.Request, error) {
-		req, err := http.NewRequest(meth, url.String(), body)
+		req, err := http.NewRequest(meth, url, body)
 		if err != nil {
 			return nil, err
 		}
